6e709e82be5d42d6b9a21afa2441da49d29a94a1
Atheeq Ur Rehman <122463167+Atheeqxcode@users.noreply.github.com>
2024-09-26 15:32:34 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
 
+const cors = require('cors');
+
+app.use(cors({
+    origin: ['https://window-shop-admin.onrender.com'], // Allow your admin domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // If dealing with cookies or authorization headers
+}));
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ShopContextProvider>
