0d2df7c84f5772ae9524adec24b4dd9f81981ae9
Atheeq Ur Rehman <122463167+Atheeqxcode@users.noreply.github.com>
2024-09-30 23:52:07 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -8,7 +8,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
  useEffect(()=>{
    //http://localhost:4000/popularinwomen
-fetch('https://window-shop-backend.onrender.com/women')
+fetch('https://window-shop-backend.onrender.com/popularinwomen')
 .then((response)=>response.json())
 .then((data)=>setPopularProducts(data));
  },[])
